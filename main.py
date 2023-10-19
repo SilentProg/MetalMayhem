e29d6689e4d925d3d2a631a7a5a21fac944bad2a
SilentProg <77963720+SilentProg@users.noreply.github.com>
2023-10-19 22:00:07 +0300

    update

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import pygame
 from modules.button import Button
 from modules.menu import Menu
 from modules.const import PATH
+
 screen = pygame.display.set_mode((800, 600))
 buttons = pygame.sprite.Group()
 
-
-
-
-
 if __name__ == "__main__":
+
+    is_running = True
+
+
+    def exit_game():
+        global is_running
+        is_running = False
+
+
     pygame.init()
     pygame.display.set_caption('Example of button')
     screen = pygame.display.set_mode((1000, 800))
     clock = pygame.time.Clock()
-
 
     # b1 = Button("CLICK ME", pos=(100, 100), command=lambda: print("clicked right now 1"), auto_size=True)
     # b2 = Button("CLICK ME 2", pos=(100, 150), command=lambda: print("clicked right now 2"), auto_size=True)
@@ -28,15 +33,13 @@ if __name__ == "__main__":
     menu.add_action('1 Player')
     menu.add_action('2 Player')
     menu.add_action('Settings')
-    menu.add_action('Exit')
+    menu.add_action('Exit', on_action=exit_game)
 
-    is_running = True
     while is_running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-
 
         # to show buttons created
 
