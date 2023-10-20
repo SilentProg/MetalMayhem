2228059567314e59d749422e5fef9e8ce7cfefea
SilentProg <77963720+SilentProg@users.noreply.github.com>
2023-10-20 16:35:43 +0300

    update Menu(Automatic height)

--- main.py
+++ main.py
@@ -3,60 +3,49 @@ import os
 import pygame
 from modules.button import Button
 from modules.menu import Menu
-from modules.const import PATH
+from modules.const import PATH, SCREEN_SIZE, WIDTH, HEIGHT
 
-screen = pygame.display.set_mode((800, 600))
-buttons = pygame.sprite.Group()
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.is_running = True
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption('Metal Mayhem: Tanktopia64 2D')
+        self.pygame_icon = pygame.image.load(os.path.join(PATH, 'assets/images/favicon.png')).convert_alpha()
+        self.pygame_icon_transformed = pygame.transform.scale(self.pygame_icon, (200, 200))
+        self.pygame_icon_transformed_rect = self.pygame_icon_transformed.get_rect(midright=(WIDTH-100, HEIGHT//2))
+        pygame.display.set_icon(self.pygame_icon)
+        self.clock = pygame.time.Clock()
+
+    def exit_game(self):
+        self.is_running = False
+
+    def play(self):
+        menu = Menu(size=(WIDTH, HEIGHT))
+        menu.add_action('1 Player')
+        menu.add_action('2 Player')
+        menu.add_action('Settings')
+        menu.add_action('Creators')
+        menu.add_action('Exit', on_action=self.exit_game)
+
+        while self.is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_running = False
+
+            menu.update()
+            menu.background.blit(self.pygame_icon_transformed, self.pygame_icon_transformed_rect)
+            self.screen.blit(menu, (0, 0))
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+def main():
+    game = Game()
+    game.play()
+
 
 if __name__ == "__main__":
-
-    is_running = True
-
-
-    def exit_game():
-        global is_running
-        is_running = False
-
-
-    pygame.init()
-    pygame.display.set_caption('Example of button')
-    screen = pygame.display.set_mode((1000, 800))
-    clock = pygame.time.Clock()
-
-    # b1 = Button("CLICK ME", pos=(100, 100), command=lambda: print("clicked right now 1"), auto_size=True)
-    # b2 = Button("CLICK ME 2", pos=(100, 150), command=lambda: print("clicked right now 2"), auto_size=True)
-    # b3 = Button("CLICK ME 3", pos=(100, 200), command=lambda: print("clicked right now 3"), auto_size=True)
-    # buttons.add(b1)
-    # buttons.add(b2)
-    # buttons.add(b3)
-    menu = Menu(size=(1000, 800))
-    menu.add_action('1 Player')
-    menu.add_action('2 Player')
-    menu.add_action('Settings')
-    menu.add_action('Creators')
-    menu.add_action('Exit', on_action=exit_game)
-
-    while is_running:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                is_running = False
-
-        # to show buttons created
-
-        screen.blit(menu, (0, 0))
-        menu.update()
-        buttons.draw(screen)
-        buttons.update()
-
-        pygame.display.update()
-        clock.tick(60)
-
-    pygame.quit()
-
-# def main():
-#     print("main")
-#
-#
-# if __name__ == '__main__':
-#     main()
+    main()
--- modules/const.py
+++ modules/const.py
@@ -3,3 +3,6 @@ import os
 import pygame
 
 PATH = os.path.abspath(__file__ + '/../..')
+WIDTH = 1000
+HEIGHT = 600
+SCREEN_SIZE = (WIDTH, HEIGHT)
